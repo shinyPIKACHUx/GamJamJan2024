253286af53f44fcd52b5c6cfe214ee7ccc37733d
waramsey <ashley.ramsey52@yahoo.com>
2024-01-24 13:46:13 -0600

    Cleaning up

--- MechDonald/Enemies/MoveTowardPlayer.gd
+++ MechDonald/Enemies/MoveTowardPlayer.gd
@@ -15,29 +15,17 @@ func _ready():
 func _physics_process(delta):
 	# Rotate toward player
 	look_at(playerRef.position, Vector3.UP)
-	#rotate_z(position.angle_to(playerRef.position))
-	#velocity = global_transform.origin.direction_to(playerRef.global_transform.origin).normalized()
-	#velocity *= delta * speed
-	
-	# Rotate velocity based on mob's rotation to move in looking direction
-	#set_velocity(velocity.rotated(Vector3.UP, rotation.y))
-	
-	#move_and_collide(velocity * delta * speed)
+
+	# Move toward player
 	position = position.move_toward(playerRef.position, delta * speed)
 
 # Called from the main scene
 func initialize(spawn_location, player):
+	# Set player reference
 	playerRef = player
 	
+	# Set position data
 	position = spawn_location
-	
-	## Rotate toward player
-	#look_at_from_position(spawn_location, player.global_transform.origin, Vector3.UP)
-	#velocity = Vector3.MODEL_FRONT * speed
-	#
-	## Rotate velocity based on mob's rotation to move in looking direction
-	##set_velocity(velocity.rotated(Vector3.UP, rotation.y))
-	#set_velocity(velocity)
 	
 func _on_visible_on_screen_enabler_3d_screen_exited():
 	queue_free();
