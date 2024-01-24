dfc22e6722404b8140be5dd4ec71b36a74c66d14
shinyPIKACHUx <mcskyx117@gmail.com>
2024-01-23 21:09:35 -0600

    fixed movement and camera
    fixed stuttering and smoothed camera and player movement. added functionality for tabbing in and out of the window.

--- MechDonald/Player/Camera.gd
+++ MechDonald/Player/Camera.gd
@@ -1,15 +1,9 @@
 extends Camera3D
 
-@onready var PlayerNode : Node3D = $".."
+
 @onready var cameraVisionTargetNode : Node3D = $"../CameraVisonTarget"
-@onready var cameraPivot : Node3D = $"../CameraPivot"
 @onready var cameraMoveTargetNode : Node3D = $"../CameraPivot/SpringArm3D/CameraMoveTarget"
 
-@export var lerp_speed : float = 0.5
-@export var lerp_scale : float = 0.1
-
-var lerp_target
-var lerp_result : Vector3
 var cameraMoveTarget : Vector3
 
 var update = false
@@ -26,10 +20,10 @@ func _ready():
 
 func update_position():
 	gp_previous = gp_current
-	gp_current = cameraMoveTarget
+	gp_current = cameraMoveTargetNode.global_position
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
+func _process(_delta):
 	if update:
 		update_position()
 		update = false
@@ -37,16 +31,7 @@ func _process(delta):
 	var f = clamp(Engine.get_physics_interpolation_fraction(), 0, 1)
 	global_position = gp_previous.lerp(gp_current, f)
 	
-	#cameraMoveTarget = cameraMoveTargetNode.global_position
-	#lerp_target = lerp(global_position, cameraMoveTarget, lerp_scale)
-	#
-	#lerp_result = lerp(global_position, lerp_target, lerp_speed * delta)
-	#
-	#global_position.x = lerp_result.x
-	#global_position.z = lerp_result.z
-	#global_position.y = lerp_result.y
 	look_at(cameraVisionTargetNode.global_position)
 
-
-func _physics_process(delta):
+func _physics_process(_delta):
 	update = true
