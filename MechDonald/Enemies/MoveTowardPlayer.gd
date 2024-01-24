a347ae612dae86504e57f86dc0bbfe9e761ddcd1
waramsey <ashley.ramsey52@yahoo.com>
2024-01-24 13:27:43 -0600

    Enemies correctly follow the player

--- MechDonald/Enemies/MoveTowardPlayer.gd
+++ MechDonald/Enemies/MoveTowardPlayer.gd
@@ -1,38 +1,43 @@
 class_name MoveTowardPlayer extends CharacterBody3D
 
-#Min speed of the mob m/s
-@export var min_speed = 10
+#speed of the mob m/s
+@export var speed = 2
 
-#Min speed of the mob m/s
-@export var max_speed = 18
-
-var player_position
-var target_position
-var player
+var playerRef
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	print("I exist!")
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta):
-	move_and_slide()
-
-# Called from the main scene
-func initialize(start_position, player_position):
-	# Place mob at start position and rotate toward player
-	look_at_from_position(start_position, player_position, Vector3.UP)
-	
-	# Rotate slightly so that it doesn't move DIRECTLY toward player
-	rotate_y(randf_range(-PI / 4, PI / 4))
-	
-	# Calculates a random speed
-	var random_speed = randi_range(min_speed, max_speed)
-	velocity = Vector3.FORWARD * random_speed
+	# Rotate toward player
+	look_at(playerRef.position, Vector3.UP)
+	#rotate_z(position.angle_to(playerRef.position))
+	#velocity = global_transform.origin.direction_to(playerRef.global_transform.origin).normalized()
+	#velocity *= delta * speed
 	
 	# Rotate velocity based on mob's rotation to move in looking direction
-	velocity = velocity.rotated(Vector3.UP, rotation.y)
+	#set_velocity(velocity.rotated(Vector3.UP, rotation.y))
+	
+	#move_and_collide(velocity * delta * speed)
+	position = position.move_toward(playerRef.position, delta * speed)
+
+# Called from the main scene
+func initialize(spawn_location, player):
+	playerRef = player
+	
+	position = spawn_location
+	
+	## Rotate toward player
+	#look_at_from_position(spawn_location, player.global_transform.origin, Vector3.UP)
+	#velocity = Vector3.MODEL_FRONT * speed
+	#
+	## Rotate velocity based on mob's rotation to move in looking direction
+	##set_velocity(velocity.rotated(Vector3.UP, rotation.y))
+	#set_velocity(velocity)
 	
 func _on_visible_on_screen_enabler_3d_screen_exited():
 	queue_free();
--- MechDonald/GameManager.gd
+++ MechDonald/GameManager.gd
@@ -13,10 +13,12 @@ func _process(delta):
 
 func _on_mob_timer_timeout():
 	# Create new mob
+	print("Timer go")
 	var mob = mob_scene.instantiate()
 	
 	var mob_spawn_location = get_node("SpawnPath/SpawnLocation")
 	mob_spawn_location.progress_ratio = randf()
 	
-	var player_position = $Player.position
-	mob.initialize(mob_spawn_location.position, player_position)
+	mob.initialize(mob_spawn_location.position, $Player)
+	
+	add_child(mob)
